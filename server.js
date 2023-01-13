f6671cd18a117d05cedc9866a8f27197b8922096
Patrick Clary <patrickm@theclarys.org>
2023-01-13 14:14:04 -0600

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+// get .env variables
+require("dotenv").config();
+
+// pull PORT from .env, give default value of 4000
+const { PORT = 4000, MONGODB_URL } = process.env;
+
+// import express
+const express = require("express");
+
+// create application object
+const app = express();
+
+// import mongoose
+const mongoose = require("mongoose");
+
+// import middlware
+const cors = require("cors");
+const morgan = require("morgan");
