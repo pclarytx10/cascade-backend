4db1235522bd6ef5d6c94bca77cc915b8e20cced
Alex Westerlund <alexwesterlund@Alexs-MacBook-Pro.local>
2023-01-16 15:42:59 -0500

    Adds seed data

---
+++ seedData.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        title: "First Post",
+        image: "",
+        content: "Welcome to Cascade, the social media site of your wildest dreams"
+    },
+    {
+        title: "Happy Monday",
+        image: "https://media2.giphy.com/media/hFmIU5GQF18Aw/giphy.gif?cid=ecf05e47c1j8jrgc6hhw5xem4u425lo3q3pao2hx2t3el10e&rid=giphy.gif&ct=g",
+        content: ""
+    },
+    {
+        title: "Did you know?",
+        image: "",
+        content: "American cheese is not originally American"
+    },
+    {
+        title: "Travels to Greece!",
+        image: "https://images.pexels.com/photos/2277653/pexels-photo-2277653.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        content: "Spent today in Santorini! My family was there too. We had the best gyros ever! I guess we'll have to come back here again!"
+    },
+    {
+        title: "Inspirational Quote",
+        image: "",
+        content: "Quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote quote."
+    },
+    {
+        title: "Did I see this right?",
+        image: "https://images.pexels.com/photos/410858/pexels-photo-410858.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        content: "An actual T-rex! I can't believe it!"
+    }
+]
